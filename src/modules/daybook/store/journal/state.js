8fc0bd95e4f80308a7a3bbfd8c4e92bc72c44e0d
karen Rios <krios@secundus.com.mx>
2023-09-08 12:41:22 -0600

    create store and adding vuex

---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,23 @@
+export default () => ({
+  isLoading: true,
+  entries: [
+    {
+      id: new Date().getTime(),
+      date: new Date().toDateString(),
+      text: "Due to using a single state tree, all states of our application are contained inside one big object. However, as our application grows in scale, the store can get really bloated.",
+      picture: null,
+    },
+    {
+      id: new Date().getTime() + 1000,
+      date: new Date().toDateString(),
+      text: "To help with that, Vuex allows us to divide our store into modules. Each module can contain its own state, mutations, actions, getters, and even nested modules - it's fractal all the way down ",
+      picture: null,
+    },
+    {
+      id: new Date().getTime() + 2000,
+      date: new Date().toDateString(),
+      text: "Similarly, inside module actions, context.state will expose the local state, and root state will be exposed as context.rootState",
+      picture: null,
+    },
+  ],
+});
