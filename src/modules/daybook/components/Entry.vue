8386543bdfeccd0996f7b546c3c06acfffb5c565
karen Rios <krios@secundus.com.mx>
2023-09-18 11:48:28 -0600

    show entry in the screen

--- src/modules/daybook/components/Entry.vue
+++ src/modules/daybook/components/Entry.vue
@@ -4,19 +4,68 @@
     @click="$router.push({ name: 'entry', params: { id: 10 } })"
   >
     <div class="entry-title d-flex">
-      <span class="text-success fs-5 fw-bold">15</span>
-      <span class="mx-1 fs-5">Julio</span>
-      <span class="mx-2 fw-light">2023, jueves</span>
+      <span class="text-success fs-5 fw-bold">{{ day }}</span>
+      <span class="mx-1 fs-5">{{ month }}</span>
+      <span class="mx-2 fw-light">{{ yearDay }}</span>
     </div>
     <div class="entry-description">
-      Un texto aleatorio es una sucesión de palabras que tienen sentido o no ,
-      que nos permite utilizarlo para rellenar texto en algún campo.
+      {{ shortText }}
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+const months = [
+  "Enero",
+  "Febrero",
+  "Marzo",
+  "Abril",
+  "Mayo",
+  "Junio",
+  "Julio",
+  "Agosto",
+  "Septiembre",
+  "Octubre",
+  "Noviembre",
+  "Diciembre",
+];
+const days = [
+  "Domingo",
+  "Lunes",
+  "Martes",
+  "Miércoles",
+  "Jueves",
+  "Viernes",
+  "Sábado",
+];
+
+export default {
+  props: {
+    entry: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    shortText() {
+      return this.entry.text.length > 130
+        ? this.entry.text.substring(0, 130) + "..."
+        : this.entry.text;
+    },
+    day() {
+      const date = new Date(this.entry.date);
+      return date.getDate();
+    },
+    month() {
+      const date = new Date(this.entry.date);
+      return months[date.getMonth()];
+    },
+    yearDay() {
+      const date = new Date(this.entry.date);
+      return `${date.getFullYear()}, ${days[date.getDay()]}`;
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
--- src/modules/daybook/views/EntryView.vue
+++ src/modules/daybook/views/EntryView.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="entry-title d-flex justify-content-between p-2">
     <div>
-      <span class="text-success fs-3 fw-bold">15</span>
-      <span class="mx-1 fs-3">Julio</span>
-      <span class="mx-4 fs-4 fw-light">2023, jueves</span>
+      <span class="text-success fs-3 fw-bold">{{ day }}</span>
+      <span class="mx-1 fs-3">{{ month }}</span>
+      <span class="mx-4 fs-4 fw-light">{{ yearDay }}</span>
     </div>
 
     <div>
@@ -21,7 +21,7 @@
 
   <hr />
   <div class="d-flex flex-column px-3 h-75">
-    <textarea placeholder="¿Que sucedio hoy?"></textarea>
+    <textarea v-model="entry.text" placeholder="¿Que sucedio hoy?"></textarea>
   </div>
   <Fab icon="fa-save" />
 
@@ -34,9 +34,50 @@
 
 <script>
 import { defineAsyncComponent } from "vue";
+import { mapGetters } from "vuex";
+
+import getDayMonthYear from "../helpers/getDayMonthYear";
+
 export default {
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+  },
   components: {
     Fab: defineAsyncComponent(() => import("../components/Fab.vue")),
+  },
+  data() {
+    return {
+      entry: null,
+    };
+  },
+  computed: {
+    ...mapGetters("journal", ["getEntryById"]),
+    day() {
+      const { day } = getDayMonthYear(this.entry.date);
+      return day;
+    },
+    month() {
+      const { month } = getDayMonthYear(this.entry.date);
+      return month;
+    },
+    yearDay() {
+      const { yearDay } = getDayMonthYear(this.entry.date);
+      return yearDay;
+    },
+  },
+  methods: {
+    loadEntry() {
+      const entry = this.getEntryById(this.id);
+      if (!entry) this.$router.push({ name: "no-entry" });
+      this.entry = entry;
+    },
+  },
+
+  created() {
+    this.loadEntry();
   },
 };
 </script>
